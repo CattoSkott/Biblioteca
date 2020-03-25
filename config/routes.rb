20f193d5790d6931838124ea604a9ef27c1649e6
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-03-25 15:46:44 -0600

    Adicion_de_BD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :loans
+  resources :tags
+  resources :favourites
+  resources :books
+  resources :shelves
+  resources :publishers
+  resources :authors
   devise_for :users
   #get 'home/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200325062505) do
+ActiveRecord::Schema.define(version: 20200325214121) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "isbn"
+    t.string "name"
+    t.string "genre"
+    t.integer "pages"
+    t.integer "year"
+    t.string "language"
+    t.integer "publisher_id"
+    t.integer "author_id"
+    t.integer "shelf_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+    t.index ["publisher_id"], name: "index_books_on_publisher_id"
+    t.index ["shelf_id"], name: "index_books_on_shelf_id"
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_favourites_on_book_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.datetime "dateloan"
+    t.datetime "datereturn"
+    t.datetime "datetoreturn"
+    t.boolean "loanstate"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_loans_on_book_id"
+    t.index ["user_id"], name: "index_loans_on_user_id"
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shelves", force: :cascade do |t|
+    t.integer "number"
+    t.string "side"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "ntag"
+    t.string "description"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_tags_on_book_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +89,7 @@ ActiveRecord::Schema.define(version: 20200325062505) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
